df77886ef42c89a1fc3bece09d643bf44ddfc42f
Kohki Sato <you@example.com>
2019-03-18 23:42:22 +0900

    Added validator to e-mail

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  onlogin(form: NgForm) {
+    console.log(form.value.email);
   }
-
 }
