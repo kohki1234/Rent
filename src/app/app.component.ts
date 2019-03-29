6117ce444413761f70a93e35a2f4acc1a67a0e7e
Kohki Sato <you@example.com>
2019-03-29 18:02:54 +0900

    Added providers pn app.component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  providers: [LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
